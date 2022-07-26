702e533ed43fd9996b3596a4ba2c2c754a5a992f
kuaidianxuexi <1461287434@qq.com>
2022-07-26 15:38:13 +0800

    first

--- src/treeSet/TreeSetTest.java
+++ src/treeSet/TreeSetTest.java
@@ -1,14 +1,23 @@
 package treeSet;
 
+import java.util.Comparator;
+import java.util.NavigableSet;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class TreeSetTest {
     public static void main(String[] args) {
-        SortedSet<String> sorter = new TreeSet<>();
-        sorter.add("Bob");
-        sorter.add("Amy");
-        sorter.add("Carl");
-        System.out.println(sorter);
+        SortedSet<Item> parts = new TreeSet<>();
+        parts.add(new Item("Toaster", 1234));
+        parts.add(new Item("Widget", 4562));
+        parts.add(new Item("Modem", 9912));
+        System.out.println(parts);
+
+        NavigableSet<Item> sortByDescription = new TreeSet<>(
+                Comparator.comparing(Item::getDescription)
+        );
+
+        sortByDescription.addAll(parts);
+        System.out.println(sortByDescription);
     }
 }
